f1041069c99f8f885f25a8e7cc36a0b2abcac180
Dannis Dayana Rincon Guevara <dannis.rincon1997@gmail.com>
2026-07-16 21:35:17 -0500

    primer parte

---
+++ logica.js
@@ -0,0 +1,5 @@
+const prompt = require ("prompt-sync")();
+let uno = Number (prompt("ingresa el primer número: "));
+let dos = Number (prompt("ingresa el segundo número: "));
+let tres = Number (prompt("ingresa el tercer número: "));
+console.log ("El número es " + uno);
