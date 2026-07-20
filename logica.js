5323549adf3ad44204f10fc864b5524996d99425
Dannis Dayana Rincon Guevara <dannis.rincon1997@gmail.com>
2026-07-19 21:01:37 -0500

    ejercicio

--- logica.js
+++ logica.js
@@ -1,5 +1,117 @@
 const prompt = require ("prompt-sync")();
-let uno = Number (prompt("ingresa el primer número: "));
-let dos = Number (prompt("ingresa el segundo número: "));
-let tres = Number (prompt("ingresa el tercer número: "));
-console.log ("El número es " + uno);
+let numbA = Number (prompt("Ingresa el primer número: "));
+let numbB = Number (prompt("Ingresa el segundo número: "));
+let numbC = Number (prompt("Ingresa el tercer número: "));
+
+// let numeroMayor = numbA;
+
+
+// if (numbA <= numbB && numbB <= numbC) {
+//     console.log("El número mayor es: " + (numbC) + ". El número del centro es: " + (numbB) + ". El número menor es: " + (numbA));
+// }
+
+
+if (numbA <= numbB && numbB <= numbC) {
+    console.log("Mayor a menor: " + (numbC) + ", " + (numbB) + ", " + (numbA));
+}
+
+
+// if (numbA <= numbC && numbC <= numbB) {
+//     console.log("El número mayor es: " + (numbB) + ". El número del centro es: " + (numbC) + ". El número menor es: " + (numbA));
+// }
+
+if (numbA <= numbC && numbC <= numbB) {
+    console.log("Mayor a menor: " + (numbB) + ", " + (numbC) + ", " + (numbA));
+}
+
+// if (numbB <= numbA && numbA <= numbC) {
+//     console.log("El número mayor es: " + (numbC) + ". El número del centro es: " + (numbA) + ". El número menor es: " + (numbB));
+// }
+
+if (numbB <= numbA && numbA <= numbC) {
+    console.log("Mayor a menor: " + (numbB) + ", " + (numbA) + ", " + (numbC));
+}
+
+
+// if (numbB <= numbC && numbC <= numbA) {  
+//     console.log ("El número mayor es: " + (numbA) + ". El número del centro es: " + (numbC) + ". El número menor es: " + (numbB));
+// }
+
+if (numbB <= numbC && numbC <= numbA) {
+    console.log("Mayor a menor: " + (numbB) + ", " + (numbC) + ", " + (numbA));
+}
+
+// if (numbC <= numbA && numbA <= numbB) {
+//     console.log ("El número mayor es: " + (numbB) + ". El número del centro es: " + (numbA) + ". El número menor es: " + (numbC))}
+
+if (numbC <= numbA && numbA <= numbB) {
+    console.log("Mayor a menor: " + (numbC) + ", " + (numbA) + ", " + (numbB));
+}
+
+// if (numbC <= numbB && numbB <= numbA) {
+//     console.log("El número mayor es: " + (numbA) + ". El número del centro es: " + (numbB) + ". El número menor es: " + (numbC));
+// }
+
+if (numbC <= numbB && numbB <= numbA) {
+    console.log("Mayor a menor: " + (numbC) + ", " + (numbB) + ", " + (numbA));
+}
+
+if (numbA === numbB && numbB === numbC) {
+    console.log("Los números son iguales");
+    
+}
+
+
+
+
+// if (numbA > numbB && numbA > numbC){
+//     console.log ("El número mayor es: " + numbA); 
+// };
+
+// if (numbB > numbC && numbB > numbA) {
+//     console.log("El número mayor es: " + numbB);
+// };
+
+// if (numbC > numbA && numbC > numbB){
+//     console.log("El número mayor es: " + numbC);
+// };
+
+
+//  if (numbA < numbB < numbC){
+//      console.log ("El número del centro es: " + numbB)
+//  };
+
+// if (numbB < numbC < numbA){
+//      console.log ("El número del centro es: " + numbC)
+//  };
+
+// if (numbC > numbA && numbA > numbB){
+//      console.log ("El número del centro es: " + numbA)
+//  };
+
+
+
+// if (numbA < numbB && numbA < numbC){
+//     console.log ("El número menor es: " + numbA); 
+// };
+
+// if (numbB < numbC && numbB < numbA) {
+//     console.log("El número menor es: " + numbB);
+// };
+
+// if (numbC < numbA && numbC < numbB){
+//     console.log("El número menor es: " + numbC);
+// };
+
+
+
+// if (uno === dos)
+//     return `los números son iguales`;
+// else (
+
+// )
+
+// console.log ("El núme   es " + uno);
+
+
+
